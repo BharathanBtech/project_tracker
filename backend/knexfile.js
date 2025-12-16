7f7f44524d8314ee103887bc984a0b31be44460b
bharathan <bharathan.thiyagaraj@tekclansolutions.com>
2025-12-16 16:59:54 +0000

    fix: fixed backend build command

---
+++ backend/knexfile.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const config = {
+    development: {
+        client: 'postgresql',
+        connection: {
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        },
+        migrations: {
+            directory: './src/database/migrations',
+            extension: 'ts',
+        },
+        seeds: {
+            directory: './src/database/seeds',
+            extension: 'ts',
+        },
+    },
+    production: {
+        client: 'postgresql',
+        connection: {
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        },
+        migrations: {
+            directory: './src/database/migrations',
+            extension: 'ts',
+        },
+        // No seeds in production
+    },
+};
+exports.default = config;
+//# sourceMappingURL=knexfile.js.map
